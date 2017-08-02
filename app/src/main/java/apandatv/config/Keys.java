c51fc90ec399ca9de97985ea277bd4cf13a3747b
1150450363kym <1150450363@qq.com>
2017-08-02 14:32:15 +0800

    重新加了友盟统计 友盟推送

--- app/src/main/java/apandatv/config/Keys.java
+++ app/src/main/java/apandatv/config/Keys.java
@@ -4,7 +4,7 @@ package apandatv.config;
  * 所有键值。。。
  * Created by lenovo on 2017/7/27.
  */
-//..
+//..ppp
 public class Keys {
     //yonghuming
     public static final String USERNAME = "userName";
@@ -12,8 +12,5 @@ public class Keys {
     public static final String JSESSIONID = "jsessionId";
     //图片验证码
     public static final String IMGCODE = "imgCode";
-
-
-
 
 }
