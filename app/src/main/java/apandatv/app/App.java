a587fa036f798da120d0c1c2a9e06e5f709541fa
1150450363kym <1150450363@qq.com>
2017-08-02 18:08:25 +0800

    小明之走在上传的路上来了

--- app/src/main/java/apandatv/app/App.java
+++ app/src/main/java/apandatv/app/App.java
@@ -2,11 +2,16 @@ package apandatv.app;
 
 import android.app.Application;
 
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.PushAgent;
+import com.umeng.socialize.PlatformConfig;
+
+import apandatv.base.BaseActivity;
+import apandatv.utils.CrashHandler;
+
 //import com.umeng.message.IUmengRegisterCallback;
 //import com.umeng.message.PushAgent;
 //import com.umeng.socialize.PlatformConfig;
-
-import apandatv.base.BaseActivity;
 
 
 /**
@@ -22,29 +27,29 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        CrashHandler.getInstance().init(this);//初始化全局异常管理
-//        PushAgent mPushAgent = PushAgent.getInstance(this);
-////注册推送服务，每次调用register方法都会回调该接口
-//        mPushAgent.register(new IUmengRegisterCallback() {
-//
-//            @Override
-//            public void onSuccess(String deviceToken) {
-//                //注册成功会返回device token
-//            }
-//
-//            @Override
-//            public void onFailure(String s, String s1) {
-//
-//            }
-//        });
+        CrashHandler.getInstance().init(this);//初始化全局异常管理
+        PushAgent mPushAgent = PushAgent.getInstance(this);
+//注册推送服务，每次调用register方法都会回调该接口
+        mPushAgent.register(new IUmengRegisterCallback() {
+
+            @Override
+            public void onSuccess(String deviceToken) {
+                //注册成功会返回device token
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+
+            }
+        });
 
 
 
     }
     {
-//        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
-//        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
-//        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
+        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
     }
 
 }
